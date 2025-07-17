b916f66200090f507a63039bbe0bf685ace2f784
Thiago Correia <thiago@Thiagos-Mac-mini.local>
2025-07-16 21:57:59 -0300

    new starting point

---
+++ screens/Onboarding.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const Onboarding = () => {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Onboarding</Text>
+        </View>
+    )
+}
+
+export default Onboarding;
